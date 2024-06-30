7435c811bcca35decc8c34bf1501943a66728605
Linus <linus_koh@mymail.sutd.edu.sg>
2024-07-01 00:27:11 +0800

    Set Up Database Cleaner

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,7 +7,7 @@
 require 'cucumber/rails'
 
 # By default, any exception happening in your Rails application will bubble up
-# to Cucumber so that your scenario will fail. This is a different from how
+# to Cucumber so that your scenario will fail. This is different from how
 # your application behaves in the production environment, where an error page will
 # be rendered instead.
 #
@@ -34,17 +34,19 @@ end
 # You may also want to configure DatabaseCleaner to use different strategies for certain features and scenarios.
 # See the DatabaseCleaner documentation for details. Example:
 #
-#   Before('@no-txn,@selenium,@culerity,@celerity,@javascript') do
-#     # { except: [:widgets] } may not do what you expect here
-#     # as Cucumber::Rails::Database.javascript_strategy overrides
-#     # this setting.
-#     DatabaseCleaner.strategy = :truncation
-#   end
-#
-#   Before('not @no-txn', 'not @selenium', 'not @culerity', 'not @celerity', 'not @javascript') do
-#     DatabaseCleaner.strategy = :transaction
-#   end
-#
+# Use truncation strategy for scenarios tagged with @no-txn, @selenium, @culerity, @celerity, or @javascript.
+# Truncation is often necessary for scenarios involving JavaScript.
+Before('@no-txn,@selenium,@culerity,@celerity,@javascript') do
+  # { except: [:widgets] } may not do what you expect here
+  # as Cucumber::Rails::Database.javascript_strategy overrides
+  # this setting.
+  DatabaseCleaner.strategy = :truncation
+end
+
+# Use transaction strategy for all other scenarios.
+Before('not @no-txn', 'not @selenium', 'not @culerity', 'not @celerity', 'not @javascript') do
+  DatabaseCleaner.strategy = :transaction
+end
 
 # Possible values are :truncation and :transaction
 # The :transaction strategy is faster, but might give you threading problems.
