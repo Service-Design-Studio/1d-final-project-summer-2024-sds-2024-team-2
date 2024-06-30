3d9265285ea93c93711f8dc1b572360633400363
Linus <linus_koh@mymail.sutd.edu.sg>
2024-06-30 12:54:09 +0800

    Signout features

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,16 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+# Pin the application JavaScript file and preload it
+pin "application", preload: true
+
+# Pin Turbo Rails and preload it
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+
+# Pin Stimulus and preload it
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+
+# Pin Stimulus loading and preload it
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+
+# Pin all JavaScript files from the controllers directory under the "controllers" namespace
 pin_all_from "app/javascript/controllers", under: "controllers"
